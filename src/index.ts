e5ddf14da13788109d5d90643b3692c227867405
Flo <florian.barbt@gmail.com>
2022-06-04 13:27:54 +0200

    fix(import hoisting): import registry example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import {instance} from "./server";
 import {Registry} from "./registry";
-import * as Example from "./registry-example";
+import "./registry-example";
+import path from "path";
+import fs from "fs";
 
 const port = 8000;
 export const app = instance();
 
-Example.setup();
 Registry.loadExpress(app, () => app.listen(port, () => console.log(`server up on ${port}`)));
 
-
+// test wasm
+const wasmDir = path.join(__dirname,'../wasm/splouch_bg.wasm');
+const wasmBuffer = fs.readFileSync(wasmDir);
+WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+   console.log((<any>wasmModule.instance.exports).fib(8));
+});
 
 
 
--- src/registry-example/index.ts
+++ src/registry-example/index.ts
@@ -4,7 +4,3 @@ export * from './greeting-service';
 export * from './toto';
 export * from './wow-logger';
 
-
-export function setup() {
-    console.log('preloading');
-}
