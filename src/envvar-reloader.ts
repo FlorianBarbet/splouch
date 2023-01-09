cae47a85510b7f64d5f51327f09028dfcee4201a
Faun <florian.barbet@ext.leroymerlin.fr>
2023-01-09 23:10:14 +0100

    let configurate the envvar future module by envvar without dotenv

--- src/envvar-reloader.ts
+++ src/envvar-reloader.ts
@@ -2,12 +2,21 @@ import dotenv from "dotenv";
 import fs from "fs";
 import {EventEmitter} from "events";
 
-dotenv.config();
+const ext  = process.env.ENV_TYPE ?? "";
+const conf_path = process.env.CONF_PATH ?? undefined;
+const path = `${(conf_path || ".env")}${ext}`;
+
+const encoding = process.env.DOT_ENV_ENCODING ?? "utf-8";
+const debug = process.env.DEBUG === 'true'
+
+
+
+dotenv.config({path, encoding, debug});
 
 let timeout: NodeJS.Timeout;
 const emitter = new EventEmitter();
 const envvar: {[name: string]:string|boolean|number} = {};
-fs.watch(".env", (event, filename) => {
+fs.watch(path || ".env", (event, filename) => {
     if(event === 'change'){
         clearTimeout(timeout);
         timeout = setTimeout(() => emitter.emit("onchange_envvar", filename), 250);
@@ -15,12 +24,12 @@ fs.watch(".env", (event, filename) => {
 });
 
 emitter.on("onchange_envvar", () => {
-    const {parsed} = dotenv.config({override: true});
+    const {parsed} = dotenv.config({override: true, path, encoding, debug});
     // let add some cast and unary operations.
     for(const key in parsed){
        const value = parsed[key];
 
-       if(value.toLowerCase().match('(true|false)')) envvar[key] = value.toLowerCase() === 'true'
+       if(value.toLowerCase().match('^(true|false)$')) envvar[key] = value.toLowerCase() === 'true'
        else {
            const numeric_value = +value
            envvar[key] = isNaN(numeric_value) ? value : numeric_value;
