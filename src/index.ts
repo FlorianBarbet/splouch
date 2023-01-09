ece0c557e09c179e514ad9f53aea7d6606645316
Faun <florian.barbet@ext.leroymerlin.fr>
2023-01-09 22:48:07 +0100

    optimize import

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import express from "express";
 import './wasm/wasm-loader';
 import './registry-example';
 import {emitter, envvar} from "./envvar-reloader";
-import {type} from "os";
+
 emitter.on("onchange_envvar", (event) => {
     console.debug(`change detected on ${event}`)
+    console.debug(envvar);
 });
 
 Registry.use(express(), {port:8000});
